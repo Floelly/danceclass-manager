687273c40129d8528d5d1d62ca1f2afb92c8d7b8
Floelly <email@florianbowitz.de>
2026-01-14 16:24:40 +0100

    added dependency to cloud service apis and added comments

--- infrastructure/network.tf
+++ infrastructure/network.tf
@@ -1,11 +1,17 @@
+# network for app
 resource "google_compute_network" "vpc_network" {
   name                    = var.vpc_name
   description             = "Only VPC network for ${data.google_project.project.name} Application"
   auto_create_subnetworks = false
   mtu                     = 1460       # Default, but important for vpc connector
   routing_mode            = "REGIONAL" # regional routes only, since app is deployed in only one region
+
+  depends_on = [
+    google_project_service.enabled_apis
+  ]
 }
 
+# private subnet for vpc access connector
 resource "google_compute_subnetwork" "vpc_subnetwork" {
   name          = "${var.vpc_name}-subnet"
   description   = "Subnet for VPC Access Connector (Connection Cloud Run -> Cloud SQL)"
