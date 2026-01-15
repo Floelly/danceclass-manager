cc8230bc84c71dc4d67ce1546842dc0f23ccb028
Floelly <email@florianbowitz.de>
2026-01-15 16:02:24 +0100

    refactor: rename

--- infrastructure/backend.tf
+++ infrastructure/backend.tf
@@ -1,11 +1,12 @@
 # service account for cloud run backend service
 resource "google_service_account" "cloud_run_sa" {
-  account_id = "clound-run-backend-sa"
+  account_id = "cloud-run-backend-sa"
+  display_name = "Cloud run backend service account"
   depends_on = [ google_project_service.enabled_apis ]
 }
 
 # rights for going through vpc access
-resource "google_project_iam_member" "cloud_run_iam_member" {
+resource "google_project_iam_member" "cloud_run_vpc_accessor" {
   member = "serviceAccount:${google_service_account.cloud_run_sa.email}"
   project = var.project_id
   role = "roles/vpcaccess.user"
