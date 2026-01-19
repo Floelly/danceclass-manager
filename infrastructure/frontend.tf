318d5ff2430e6f26400685694cd5435d7ff2a311
Floelly <email@florianbowitz.de>
2026-01-19 14:47:30 +0100

    added GCS storage bucket for static frontend files

--- infrastructure/frontend.tf
+++ infrastructure/frontend.tf
@@ -1,7 +1,7 @@
 # Storage Bucket
 resource "google_storage_bucket" "frontend" {
   name          = "${var.project_id}-frontend"
-  location = var.region
+  location      = var.region
   force_destroy = true
 
   versioning {
@@ -13,7 +13,7 @@ resource "google_storage_bucket" "frontend" {
   }
   
   uniform_bucket_level_access = true
-  depends_on = [ google_project_service.enabled_apis ]
+  depends_on                  = [google_project_service.enabled_apis]
 }
 
 # Public Access
@@ -30,7 +30,7 @@ resource "google_compute_global_address" "frontend_ip" {
 
 # Backend Bucket
 resource "google_compute_backend_bucket" "frontend" {
-  name             = "frontend-backend-bucket"
-  bucket_name      = google_storage_bucket.frontend.name
-  enable_cdn       = true
+  name        = "frontend-backend-bucket"
+  bucket_name = google_storage_bucket.frontend.name
+  enable_cdn  = true
 }
